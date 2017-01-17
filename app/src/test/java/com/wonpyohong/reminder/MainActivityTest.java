faa4d068e74bd77bda7e1cb8839e3ce0131fb09b
wonpyohong <wonpyo.hong@navercorp.com>
2017-01-17 15:31:41 +0900

    title 테스트 추가.

--- app/src/test/java/com/wonpyohong/reminder/MainActivityTest.java
+++ app/src/test/java/com/wonpyohong/reminder/MainActivityTest.java
@@ -6,12 +6,31 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import android.app.Activity;
+import junit.framework.Assert;
+
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 21,
 	constants = BuildConfig.class)
 public class MainActivityTest {
 	@Test
 	public void onCreateTest() throws Exception {
+		// given
+
+		// when
 		Robolectric.setupActivity(MainActivity.class);
+
+		// then
+	}
+
+	@Test
+	public void title_is_Reminder() {
+		// given
+
+		// when
+		Activity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+		// then
+		Assert.assertEquals(mainActivity.getTitle(), "Reminder");
 	}
 }
